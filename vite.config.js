7b315a36af2a9d5e95db804163f51c838c032cd1
VamsiKLU <jakkulavamsi7@gmail.com>
2025-08-26 13:34:01 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/HMS/',  // REPLACE with your repo name
+  base: '/HMS/',   // change this to your repo name
   plugins: [react()],
-  css: { postcss: './postcss.config.cjs' }
+  css: {
+    postcss: './postcss.config.cjs'
+  }
 })
